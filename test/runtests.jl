fe4ae23e39f9f993d8c27e327a4c276ef16b71e7
Josh Day <emailjoshday@gmail.com>
2024-02-25 07:19:51 -0500

    ci debugging

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,8 +6,22 @@ depot = DepotDelivery.build(joinpath(@__DIR__, "TestProject"))
 
 @test DepotDelivery.test(depot)
 
+# debugging artifacts in CI
+function print_file_tree(path, depth=0)
+    if isfile(path)
+        printstyled("    " ^ depth, basename(path), '\n', color=:light_blue)
+    else
+        println("    " ^ depth, basename(path), "/")
+        for f in readdir(path)
+            print_file_tree(joinpath(path, f), depth + 1)
+        end
+    end
+end
+
+
 DepotDelivery.sandbox() do
     include(joinpath(depot, "config", "depot_startup.jl"))
+    print_file_tree(joinpath(depot, "artifacts"))  # debugging artifacts in CI
     @test !occursin(".julia", pathof(TestProject))
     @test !occursin(".julia", pathof(TestProject.HDF5))
 end
