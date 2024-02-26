54847ff7bea83330f4fa98f4654630eae90bb854
Josh Day <emailjoshday@gmail.com>
2024-02-26 06:26:20 -0500

    figured out ci woes

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,26 +6,9 @@ depot = DepotDelivery.build(joinpath(@__DIR__, "TestProject"))
 
 @test DepotDelivery.test(depot)
 
-# debugging artifacts in CI
-function print_file_tree(path, depth=0)
-    if isfile(path)
-        printstyled("    " ^ depth, basename(path), '\n', color=:light_blue)
-    else
-        println("    " ^ depth, basename(path), "/")
-        for f in readdir(path)
-            print_file_tree(joinpath(path, f), depth + 1)
-        end
-    end
-end
-
-
-
 DepotDelivery.sandbox() do
-    @info isdir(depot)
-    @info readdir(depot)
-    print_file_tree(joinpath(depot, "artifacts"))  # debugging artifacts in CI
-
     include(joinpath(depot, "config", "depot_startup.jl"))
     @test !occursin(".julia", pathof(TestProject))
     @test !occursin(".julia", pathof(TestProject.HDF5))
+    @test !occursin(".julia", pathof(TestProject.HDF5.API.HDF5_jll))
 end
