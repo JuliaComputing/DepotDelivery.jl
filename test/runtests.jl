cb07ae96a9e7d441dc98264bf731d77d9a5fa9d9
Elvis Aguero <elvis.alexander.aguero.vera@cern.ch>
2024-07-31 15:25:36 +0200

    Added test for multiple applications coexisting
    Added two test sets to test that the packages were installed correctly and also that the cache files exist in the host machine.
    The test consists on installing and precompiling three packages:
    URIs, Unzip and BitFlags that exist in different Project.toml files
    inside the directory MultipleWorkflows/

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,3 +18,30 @@ end
 depot2 = DepotDelivery.build(joinpath(@__DIR__, "TestProject"), platform = Pkg.BinaryPlatforms.Windows(:x86_64))
 
 path = joinpath(depot2, "packages", "HDF5_jll")
+
+
+#------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------
+# Testing multiple workflows
+packages_list = readdir("MultipleWorkflows/");
+proj_paths = joinpath.("./MultipleWorkflows/", packages_list);
+depot = DepotDelivery.build(proj_paths, precompiled=true)
+
+DepotDelivery.sandbox() do 
+    push!(empty!(DEPOT_PATH), depot)
+    
+    # Test that for every project instantiated, their dependencies exist 
+    # and the depot path does not point to the default value
+    @testset for (proj, package) in zip(proj_paths, packages_list)
+        Pkg.activate(proj)
+        package_symbol = Symbol(package)
+        @eval using $package_symbol
+        package_value = eval(package_symbol)
+        @test !occursin(".julia", pathof(package_value))
+    end
+
+    # Ensure compiled folders are populated
+    @testset for package in packages_list
+        @test length(readdir(joinpath(depot, "compiled", "v$(VERSION.major).$(VERSION.minor)", package))) > 1
+    end
+end
